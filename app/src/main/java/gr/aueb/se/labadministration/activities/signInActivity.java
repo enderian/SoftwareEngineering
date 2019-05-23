c556ed5e97a316c7766b12a701ffea9213ca92f9
Konstantinos <koslamprakis@gmail.com>
2019-05-23 11:24:45 +0300

    new package, activity Sign In

---
+++ app/src/main/java/gr/aueb/se/labadministration/activities/signInActivity.java
@@ -0,0 +1,14 @@
+package gr.aueb.se.labadministration.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import gr.aueb.se.labadministration.R;
+
+public class signInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
