3c2b2f6776f2e04b8ccb8f3d64c7157e54878328
MrGoumX <p3160026@dias.aueb.gr>
2019-05-27 00:38:51 +0300

    Started writing JavaDoc

--- app/src/main/java/gr/aueb/se/labadministration/dao/LaboratoryDAO.java
+++ app/src/main/java/gr/aueb/se/labadministration/dao/LaboratoryDAO.java
@@ -4,13 +4,33 @@ import gr.aueb.se.labadministration.domain.lab.Laboratory;
 
 import java.util.ArrayList;
 
+/**
+ * The interface the communicates with databases for the laboratories
+ */
 public interface LaboratoryDAO {
 
+    /**
+     * The method that saves a lab to the database
+     * @param laboratory
+     */
     void save(Laboratory laboratory);
 
+    /**
+     * The method that removes a lab from the database
+     * @param laboratory
+     */
     void remove(Laboratory laboratory);
 
+    /**
+     * The method that searches if a lab exists with that name
+     * @param name
+     * @return a lab or null
+     */
     Laboratory findByName(String name);
 
+    /**
+     * The method that returns all the labs
+     * @return
+     */
     ArrayList<Laboratory> listAll();
 }
